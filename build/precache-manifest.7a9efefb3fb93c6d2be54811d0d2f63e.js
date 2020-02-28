6947e840e476300ce75af71625cd54ba8ad41616
Akadar.akku <akadar.akku121@gmail.com>
2020-02-28 20:39:05 +0100

    exercise week 4 gh-pages

---
+++ build/precache-manifest.7a9efefb3fb93c6d2be54811d0d2f63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2f31fb1c1226fcd78e3ef9f35691c8",
+    "url": "/week-exercises-app/index.html"
+  },
+  {
+    "revision": "3d1ec82a1f5966af0f9c",
+    "url": "/week-exercises-app/static/css/main.31acd513.chunk.css"
+  },
+  {
+    "revision": "408bf4e7102531ec22de",
+    "url": "/week-exercises-app/static/js/2.bbc82831.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/week-exercises-app/static/js/2.bbc82831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1ec82a1f5966af0f9c",
+    "url": "/week-exercises-app/static/js/main.f9a439dc.chunk.js"
+  },
+  {
+    "revision": "3efe2b585a492a5314d0",
+    "url": "/week-exercises-app/static/js/runtime-main.725deb20.js"
+  }
+]);
